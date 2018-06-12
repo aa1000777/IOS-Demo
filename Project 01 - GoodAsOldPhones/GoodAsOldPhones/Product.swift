3a5f96da610f266a34fce369f3f91699b90da957
aa1000777 <aa1000777@qq.com>
2018-06-12 18:06:32 +0800

    Project 01 - GoodAsOldPhones

---
+++ Project 01 - GoodAsOldPhones/GoodAsOldPhones/Product.swift
@@ -0,0 +1,21 @@
+//
+//  Product.swift
+//  GoodAsOldPhones
+//
+//  Copyright © 2016 Code School. All rights reserved.
+//
+
+import Foundation
+
+class Product {
+  var name: String?
+  var cellImageName: String?
+  var fullscreenImageName: String?
+  var msg:String="这是我要传的参数"
+  
+  init(name: String, cellImageName: String, fullscreenImageName: String) {
+    self.name = name
+    self.cellImageName = cellImageName
+    self.fullscreenImageName = fullscreenImageName
+  }
+}
